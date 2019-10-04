7e917c4f3fb50ca36700cde37c28a499265d849b
Bryan <dev02@nb02.de>
2019-10-04 17:46:48 +0200

    highlight route when 2 stations are chosen

--- e_metrobus/static/js/project.js
+++ e_metrobus/static/js/project.js
@@ -3,6 +3,8 @@
 
 /* ROUTE */ 
 let stations = [];
+let routeShow = [];
+let stationsShow = [];
 
 // add station ID to array
 function saveStation(id) {
@@ -25,6 +27,46 @@ function highlightStation(id) {
   $(station + " svg").toggle();
 }
 
+function highlightSVG(stationID) {
+  for (i = 0; i < stationID.length; i++) {
+    $(stationID[i] + " rect").css({fill: 'red'});
+  }
+}
+
+// highlight line route
+function showRoute(e) {
+  for (i = e[0]; i < e[1]; i++) {
+    routeShow.push("#stationLine" + i);
+  }
+  highlightSVG(routeShow);
+  return routeShow;
+}
+
+// highlight line route
+function showStations(e) {
+  for (i = e[0]; i < e[1]; i++) {
+    stationsShow.push("#betweenStations" + i);
+  }
+  highlightSVG(stationsShow);
+  return stationsShow;
+}
+
+// return station ID numbers
+function getStationsID(e) {
+  for (i = 0; i < e.length; i++) {
+    e[i] = e[i].slice(-2);
+    // remove 0 at beginning of ID number if there
+    if (e[i][0] === "0") {
+      e[i] = e[i].slice(-1);
+    }
+    e[i] = parseInt(e[i]);
+  }
+  console.log("stations ID numbers: " + e);
+  showRoute(e);
+  showStations(e);
+  //return e;
+}
+
 $("#stationsList .station").click(function() {
 	// if no station is selected yet -> add departure station
 	if (stations.length === 0) {
@@ -43,6 +85,7 @@ $("#stationsList .station").click(function() {
   	//console.log("Array 1 is not same");
     saveStation(this.id);
     highlightStation(this.id);
+    getStationsID(stations);
   }
   // if departure and arrival stations are selected and arrival station is tapped again -> remove arrival station
   else if (stations.length === 2 && stations[1] === this.id) {
@@ -53,5 +96,7 @@ $("#stationsList .station").click(function() {
   else {
   	//console.log("error");
   }
+
+
 });
 //console.log("stations: " + stations);
