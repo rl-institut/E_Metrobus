3b0b4b199205606447929e78f2ae3306692815c8
hendrik <hendrik.huyskens@rl-institut.de>
2019-10-08 14:28:25 +0200

    Fixed: Stations are not overwritten by getStationsID

--- e_metrobus/static/js/project.js
+++ e_metrobus/static/js/project.js
@@ -1,7 +1,7 @@
 /* Project specific Javascript goes here. */
 
 
-/* ROUTE */ 
+/* ROUTE */
 let stations = [];
 let routeShow = [];
 let stationsShow = [];
@@ -10,14 +10,12 @@ let stationsShow = [];
 function saveStation(id) {
 	stations.push(id);
     console.log(stations);
-	return stations;
 }
 
 // remove station ID from array
 function removeStation(id) {
 	stations.splice(-1,1);
     console.log(stations);
-	return stations;
 }
 
 // de-/highlight station with background-color
@@ -40,7 +38,6 @@ function showRoute(e) {
     routeShow.push("#stationLine" + i);
   }
   highlightSVG(routeShow);
-  return routeShow;
 }
 
 // highlight line route (between stations)
@@ -49,23 +46,22 @@ function showStations(e) {
     stationsShow.push("#betweenStations" + i);
   }
   highlightSVG(stationsShow);
-  return stationsShow;
 }
 
 // return station ID numbers without "station"
 function getStationsID(e) {
+  let a = [];
   for (i = 0; i < e.length; i++) {
-    e[i] = e[i].slice(-2);
+    a[i] = e[i].slice(-2);
     // remove 0 at beginning of ID number if there
-    if (e[i][0] === "0") {
-      e[i] = e[i].slice(-1);
+    if (a[i][0] === "0") {
+      a[i] = a[i].slice(-1);
     }
-    e[i] = parseInt(e[i]);
+    a[i] = parseInt(a[i]);
   }
-  console.log("stations ID numbers: " + e);
-  showRoute(e);
-  showStations(e);
-  //return e;
+  console.log("stations ID numbers: " + a);
+  showRoute(a);
+  showStations(a);
 }
 
 $("#stationsList .station").click(function() {
@@ -86,7 +82,7 @@ $("#stationsList .station").click(function() {
   	//console.log("Array 1 is not same");
     saveStation(this.id);
     highlightStation(this.id);
-    //getStationsID(stations);
+    getStationsID(stations);
     console.log("stations[0]: " + stations[0]);
   }
   // if departure and arrival stations are selected and arrival station is tapped again -> remove arrival station
