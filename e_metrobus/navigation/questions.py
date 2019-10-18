c44d78814c89a9a635281fb2027db799c5936a13
hendrik <hendrik.huyskens@rl-institut.de>
2019-10-18 11:17:37 +0200

    Questions are read in from config file

--- e_metrobus/navigation/questions.py
+++ e_metrobus/navigation/questions.py
@@ -1,35 +1,42 @@
-
-from typing import List
+import os
+from typing import List, Dict
 from dataclasses import dataclass
+from configobj import ConfigObj
+
+from django.conf import settings
 
 
 SCORE_WRONG = 5
 SCORE_CORRECT = 10
-SCORE_CATEGORY_COMPLETE = 10
+SCORE_CATEGORY_COMPLETE = 11
 
 
 @dataclass
 class Question:
-    name: str
-    url: str
+    question: str
+    answers: List[str]
+    correct: int
+    template: str
 
 
 @dataclass
 class Category:
-    name: str
-    questions: List[Question]
+    label: str
+    questions: Dict[str, Question]
 
 
-QUESTIONS = [
-    Category(
-        name='Ich',
-        questions=[Question(f'Q{i}', f'qurl_{i}') for i in range(3)]
-    ),
-    Category(
-        name='Umwelt',
-        questions=[Question(f'Q{i}', f'qurl_{i}') for i in range(3)]
-    )
-]
+question_config = ConfigObj(
+    os.path.join(settings.APPS_DIR, "navigation", "questions.cfg")
+)
+QUESTIONS = {}
+
+for cat in question_config:
+    questions = {}
+    for q in question_config[cat]["questions"]:
+        questions[q] = Question(
+            template=f"{cat}/{q}.html", **question_config[cat]["questions"][q]
+        )
+    QUESTIONS[cat] = Category(label=question_config[cat]["label"], questions=questions)
 
 
 def get_points_for_category(category: str, session):
