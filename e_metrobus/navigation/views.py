17f7c2514140058c8a33847b4178b3686486671e
hendrik <hendrik.huyskens@rl-institut.de>
2019-11-06 13:50:12 +0100

    Implemented first draft of dashboard (no layout)

--- e_metrobus/navigation/views.py
+++ e_metrobus/navigation/views.py
@@ -1,8 +1,8 @@
-
 from django.views.generic import TemplateView
 from django.shortcuts import redirect
 
 from e_metrobus.navigation import widgets, constants, questions
+
 
 class NavigationView(TemplateView):
     title = "E-Metrobus"
@@ -35,35 +35,45 @@ class RouteView(TemplateView):
     def get(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
 
-        context['stations'] = widgets.StationsWidget(constants.STATIONS, request)
+        context["stations"] = widgets.StationsWidget(constants.STATIONS, request)
         return self.render_to_response(context)
 
     def post(self, request, *args, **kwargs):
         def get_stations():
             stations_raw = request.POST["stations"]
-            start, end = stations_raw.split(',')
+            start, end = stations_raw.split(",")
             return int(start[-2:]) - 1, int(end[-2:]) - 1
 
-        request.session['stations'] = get_stations()
-        return redirect('navigation:comparison')
+        request.session["stations"] = get_stations()
+        return redirect("navigation:comparison")
 
 
 class ComparisonView(NavigationView):
     template_name = "navigation/comparison.html"
 
     def get(self, request, *args, **kwargs):
-        stations = request.session['stations']
+        stations = request.session["stations"]
         context = self.get_context_data(stations, **kwargs)
         return self.render_to_response(context)
 
     def get_context_data(self, stations, **kwargs):
         context = super(ComparisonView, self).get_context_data(**kwargs)
-        context['stations'] = [constants.STATIONS[i] for i in stations]
+        context["stations"] = [constants.STATIONS[i] for i in stations]
         return context
 
 
 class DashboardView(NavigationView):
     template_name = "navigation/dashboard.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(DashboardView, self).get_context_data(**kwargs)
+        context["categories"] = {
+            category: questions.get_category_done_share(
+                category, self.request.session
+            ) * 100
+            for category in questions.QUESTIONS
+        }
+        return context
 
 
 class LandingPageView(TemplateView):
