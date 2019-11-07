8c2fcfbaf504e0551acb3be5491abe8226f78b0e
Bryan <dev02@nb02.de>
2019-11-07 17:25:25 +0100

    start animation test

--- e_metrobus/static/js/project.js
+++ e_metrobus/static/js/project.js
@@ -1,2 +1,13 @@
 /* Project specific Javascript goes here. */
 
+// Animate onboarding part
+
+setTimeout(function() {
+  $('#onboardingAnimate0').hide();
+  $('#onboardingAnimate1').show();
+}, 2000);
+
+// setTimeout(function() {
+//   $('#onboardingAnimate1').hide();
+//   $('#onboardingAnimate2').show();
+// }, 4000);
