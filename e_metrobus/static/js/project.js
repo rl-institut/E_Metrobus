f20362016167aafeefc136150aaa925134630c87
Bryan <dev02@nb02.de>
2019-11-08 16:46:45 +0100

    implement basic animation

--- e_metrobus/static/js/project.js
+++ e_metrobus/static/js/project.js
@@ -2,12 +2,33 @@
 
 // Animate onboarding part
 
+// hide route
+// show yellow circle
+// show first result
 setTimeout(function() {
-  $('#onboardingAnimate0').hide();
+  $('#onboardingAnimateRoute').hide();
   $('#onboardingAnimateBackground').show();
+  $('#onboardingAnimateText1').show();
 }, 2000);
 
-// setTimeout(function() {
-//   $('#onboardingAnimate1').hide();
-//   $('#onboardingAnimate2').show();
-// }, 4000);
+setTimeout(function() {
+  $('#onboardingAnimateText1').hide();
+  $('#onboardingAnimateText2').show();
+}, 4000);
+
+setTimeout(function() {
+  $('#onboardingAnimateText2').hide();
+  $('#onboardingAnimateText3').show();
+}, 6000);
+
+setTimeout(function() {
+  $('#onboardingAnimateText3').hide();
+  $('#onboardingAnimateText4').show();
+}, 8000);
+
+setTimeout(function() {
+  $('#onboardingAnimateBackground').hide();
+  $('#onboardingAnimateText4').hide();
+  $('#onboardingAnimateChart').show();
+  console.log('hey');
+}, 10000);
