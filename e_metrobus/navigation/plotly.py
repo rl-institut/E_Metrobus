e574ed671dfbbebb802636cd5516806a0566365d
hendrik <hendrik.huyskens@rl-institut.de>
2019-11-12 14:16:34 +0100

    started to integrate plotly

---
+++ e_metrobus/navigation/plotly.py
@@ -0,0 +1,5 @@
+
+import plotly
+import plotly.graph_objects as go
+
+class DjangoFigure(go.Figure):
--- e_metrobus/navigation/views.py
+++ e_metrobus/navigation/views.py
@@ -1,8 +1,11 @@
 
+import plotly
+import plotly.graph_objects as go
+
 from django.views.generic import TemplateView
 from django.shortcuts import redirect
 
-from e_metrobus.navigation import widgets, constants
+from e_metrobus.navigation import constants
 
 from e_metrobus.navigation import widgets
 
@@ -69,3 +72,24 @@ class LandingPageView(TemplateView):
     template_name = "navigation/landing-page.html"
     # Example config
     footer_links = {'pin': {'enabled': False}, 'info': {'selected': True}}
+
+
+class PlotlyView(TemplateView):
+    template_name = "navigation/plotly.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(PlotlyView, self).get_context_data(**kwargs)
+
+        # Plotly Figure:
+        animals = ['giraffes', 'orangutans', 'monkeys']
+        fig = go.Figure([go.Bar(x=animals, y=[20, 14, 23])])
+        fig.layout.width = None
+        fig.layout.height = None
+        plotly_div = plotly.offline.plot(fig, include_plotlyjs=False, output_type="div")
+        context["plotly"] = plotly_div
+        script = plotly_div[plotly_div.find("<script") + len(
+            '<script type="text/javascript">'):plotly_div.find("</script>")]
+        div_id_start = plotly_div.find('<div id="') + 9
+        div_id_end = plotly_div.find('"', div_id_start)
+        context["plotly_id"] = plotly_div[div_id_start:div_id_end]
+        return context
