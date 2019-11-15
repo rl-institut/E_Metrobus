9732e6bf3eb3e837355484583c7a0252cf7fe8fe
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2019-11-15 10:31:51 +0100

    Refactored comparison view into display route

--- e_metrobus/navigation/urls.py
+++ e_metrobus/navigation/urls.py
@@ -6,7 +6,6 @@ app_name = "navigation"
 urlpatterns = [
     path("", view=views.LandingPageView.as_view(), name="landing-page"),
     path("route/", view=views.RouteView.as_view(), name="route"),
-    path("comparison/", view=views.ComparisonView.as_view(), name="comparison"),
     path("dashboard/", view=views.DashboardView.as_view(), name="dashboard"),
     path("display_route/", view=views.DisplayRouteView.as_view(), name="display_route"),
 ]
