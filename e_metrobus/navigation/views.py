75b01d3a228345830f27f32fb76cfbc382b18645
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2019-11-21 17:10:25 +0100

    Added images to chart (not yet positioned)

--- e_metrobus/navigation/views.py
+++ e_metrobus/navigation/views.py
@@ -1,4 +1,3 @@
-
 import plotly
 import plotly.graph_objects as go
 
@@ -20,14 +19,14 @@ class NavigationView(TemplateView):
     def get_context_data(self, **kwargs):
         points = self.request.session.get("points", 0)
         return {
-            'footer': widgets.FooterWidget(links=self.footer_links),
-            'top_bar': widgets.TopBarWidget(
+            "footer": widgets.FooterWidget(links=self.footer_links),
+            "top_bar": widgets.TopBarWidget(
                 title=self.title,
                 title_icon=self.title_icon,
                 title_alt=self.title_alt,
                 back_url=self.back_url,
-                points=points
-            )
+                points=points,
+            ),
         }
 
 
@@ -37,30 +36,30 @@ class RouteView(TemplateView):
     def get(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
 
-        context['stations'] = widgets.StationsWidget(constants.STATIONS, request)
+        context["stations"] = widgets.StationsWidget(constants.STATIONS, request)
         return self.render_to_response(context)
 
     def post(self, request, *args, **kwargs):
         def get_stations():
             stations_raw = request.POST["stations"]
-            start, end = stations_raw.split(',')
+            start, end = stations_raw.split(",")
             return int(start[-2:]) - 1, int(end[-2:]) - 1
 
-        request.session['stations'] = get_stations()
-        return redirect('navigation:comparison')
+        request.session["stations"] = get_stations()
+        return redirect("navigation:comparison")
 
 
 class ComparisonView(NavigationView):
     template_name = "navigation/comparison.html"
 
     def get(self, request, *args, **kwargs):
-        stations = request.session['stations']
+        stations = request.session["stations"]
         context = self.get_context_data(stations, **kwargs)
         return self.render_to_response(context)
 
     def get_context_data(self, stations, **kwargs):
         context = super(ComparisonView, self).get_context_data(**kwargs)
-        context['stations'] = [constants.STATIONS[i] for i in stations]
+        context["stations"] = [constants.STATIONS[i] for i in stations]
         return context
 
 
@@ -71,7 +70,7 @@ class DashboardView(NavigationView):
 class LandingPageView(TemplateView):
     template_name = "navigation/landing-page.html"
     # Example config
-    footer_links = {'pin': {'enabled': False}, 'info': {'selected': True}}
+    footer_links = {"pin": {"enabled": False}, "info": {"selected": True}}
 
 
 class PlotlyView(TemplateView):
@@ -80,17 +79,32 @@ class PlotlyView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(PlotlyView, self).get_context_data(**kwargs)
 
+        colors = ["Gainsboro",] * 5
+        colors[2] = "black"
+
         # Plotly Figure:
-        animals = ['giraffes', 'orangutans', 'monkeys']
-        fig = go.Figure([go.Bar(x=animals, y=[20, 14, 23])])
+        mobiles = ["Zu Fuß", "Fahrrad", "E-Bus", "Dieselbus", "PKW"]
+        fig = go.Figure([go.Bar(x=mobiles, y=[5, 5, 20, 30, 40], marker_color=colors)])
         fig.layout.margin.t = 0
         fig.layout.margin.b = 0
         fig.layout.margin.l = 0
         fig.layout.margin.r = 0
         fig.layout.autosize = True
-        fig.layout.plot_bgcolor = '#fff'
-        # fig.layout.colorscale = [[0, plotly.colors.rgb(220,220,220)], [0.2, rgb(245,195,157)], [0.4, rgb(245,160,105)], [1, rgb(178,10,28)], ]
+        fig.layout.plot_bgcolor = "#fff"
         fig.layout.yaxis.visible = False
+
+        fig.add_layout_image(
+            go.layout.Image(source="/static/images/icons/i_walk.svg", x=0, y=0.1)
+        )
+        fig.add_layout_image(
+            go.layout.Image(source="/static/images/icons/i_bus.svg", x=1, y=0.1)
+        )
+        fig.update_layout_images(
+            dict(
+                xref="x", yref="paper", sizex=0.2, sizey=0.2, xanchor="right", yanchor="bottom"
+            )
+        )
+
         dj_fig = utils.DjangoFigure(fig, displayModeBar=False)
         context["plotly"] = dj_fig
         return context
