28d262a64522967f1aaa602e73b2dc1e25177b9a
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2019-11-28 12:07:14 +0100

    Fixed display route url

--- e_metrobus/navigation/urls.py
+++ e_metrobus/navigation/urls.py
@@ -6,6 +6,7 @@ app_name = "navigation"
 urlpatterns = [
     path("", view=views.LandingPageView.as_view(), name="landing-page"),
     path("route", view=views.RouteView.as_view(), name="route"),
+    path("display_route/", view=views.DisplayRouteView.as_view(), name="display_route"),
     path("comparison", view=views.ComparisonView.as_view(), name="comparison"),
     path("dashboard", view=views.DashboardView.as_view(), name="dashboard"),
     path("quiz/<str:category>", view=views.QuestionView.as_view(), name="question"),
