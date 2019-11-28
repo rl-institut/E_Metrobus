3ee4d204988ad73d729e072aadbb5522721d0232
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2019-11-28 15:51:12 +0100

    Added backslashes to urls

--- e_metrobus/navigation/urls.py
+++ e_metrobus/navigation/urls.py
@@ -5,14 +5,14 @@ from e_metrobus.navigation import views
 app_name = "navigation"
 urlpatterns = [
     path("", view=views.LandingPageView.as_view(), name="landing-page"),
-    path("route", view=views.RouteView.as_view(), name="route"),
+    path("route/", view=views.RouteView.as_view(), name="route"),
     path("display_route/", view=views.DisplayRouteView.as_view(), name="display_route"),
-    path("comparison", view=views.ComparisonView.as_view(), name="comparison"),
-    path("dashboard", view=views.DashboardView.as_view(), name="dashboard"),
-    path("quiz/<str:category>", view=views.QuestionView.as_view(), name="question"),
-    path("answer", view=views.AnswerView.as_view(), name="answer"),
+    path("comparison/", view=views.ComparisonView.as_view(), name="comparison"),
+    path("dashboard/", view=views.DashboardView.as_view(), name="dashboard"),
+    path("quiz/<str:category>/", view=views.QuestionView.as_view(), name="question"),
+    path("answer/", view=views.AnswerView.as_view(), name="answer"),
     path(
-        "questions_as_text",
+        "questions_as_text/",
         view=views.QuestionsAsTextView.as_view(),
         name="questions_as_text",
     ),
