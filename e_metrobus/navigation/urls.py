6556086b3605781aa2cf904e498bc7d3938c730a
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2019-12-10 14:54:38 +0100

    Minor change

--- e_metrobus/navigation/urls.py
+++ e_metrobus/navigation/urls.py
@@ -4,7 +4,7 @@ from e_metrobus.navigation import views
 
 app_name = "navigation"
 urlpatterns = [
-    path("", view=views.LandingPageView.as_view(), name="landing-page"),
+    path("", view=views.LandingPageView.as_view(), name="landing_page"),
     path("route/", view=views.RouteView.as_view(), name="route"),
     path("display_route/", view=views.DisplayRouteView.as_view(), name="display_route"),
     path("comparison/", view=views.ComparisonView.as_view(), name="comparison"),
