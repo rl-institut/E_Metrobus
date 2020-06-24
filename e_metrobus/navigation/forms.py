b311c0d4c42163090986561ec640a38994171e16
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2020-06-24 14:19:42 +0200

    Implemented feedback and bug form on legal page
    Removed feedback from quiz finished page

--- e_metrobus/navigation/forms.py
+++ e_metrobus/navigation/forms.py
@@ -10,22 +10,13 @@ from e_metrobus.navigation.widgets import FeedbackCommentWidget, FeedbackStarsWi
 class FeedbackForm(forms.ModelForm):
     class Meta:
         model = Feedback
-        fields = "__all__"
+        fields = ("comment",)
         labels = {
-            "question1": "Frage 1:",
-            "question2": "Frage 2:",
-            "question3": "Frage 3:",
             "comment": "Schreib uns Deine Meinung/Kritik:",
         }
         widgets = {
-            "question1": FeedbackStarsWidget,
-            "question2": FeedbackStarsWidget,
-            "question3": FeedbackStarsWidget,
             "comment": FeedbackCommentWidget
         }
-
-    class Media:
-        js = ("js/feedback.js",)
 
 
 class BugForm(forms.ModelForm):
