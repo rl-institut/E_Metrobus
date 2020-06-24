95c52744fd52d8862af9e1609454216408f99df4
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2020-06-24 13:50:15 +0200

    Fixed dashboard footer image on question page

--- e_metrobus/navigation/views.py
+++ e_metrobus/navigation/views.py
@@ -213,7 +213,7 @@ class QuestionView(NavigationView):
     back_url = "navigation:dashboard"
     footer_links = {
         "info": {"enabled": True},
-        "dashboard": {"selected": True, "enabled": True},
+        "dashboard": {"selected": True},
         "leaf": {"enabled": True},
         "results": {"enabled": True},
     }
