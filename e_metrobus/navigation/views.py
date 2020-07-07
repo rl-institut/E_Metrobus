ef39eec7c597c483d7ab6b9eb2791b1205435a3d
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2020-07-07 11:21:57 +0200

    Removed first placeholder question

--- e_metrobus/navigation/views.py
+++ e_metrobus/navigation/views.py
@@ -177,10 +177,6 @@ class DisplayRouteView(CheckStationsMixin, NavigationView):
         # Set first question as "answered":
         if "questions" not in request.session:
             request.session["questions"] = {}
-        if "e_metrobus" not in request.session["questions"]:
-            request.session["questions"]["e_metrobus"] = {}
-        request.session["questions"]["e_metrobus"]["route"] = True
-        request.session["last_answered_question"] = "route"
         request.session.save()
         return super(DisplayRouteView, self).get(request, *args, **kwargs)
 
